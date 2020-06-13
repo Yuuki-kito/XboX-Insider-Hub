bdc73b343aea687412b85e205d0a38553a4df725
Yuuki <62210133+Yuuki-kito@users.noreply.github.com>
2020-06-13 09:50:35 +0700

    Rebuild 1.2
    - Fixed apps not installed because the cert was executed after add-appxpackage command.
    - Fixed Xbox not being installed because Runtime was missing.
    - Added Runtime to XboX Insider to work.
    - Added warning if needed files were missing.

--- Add-XboX.cmd
+++ Add-XboX.cmd
@@ -2,7 +2,13 @@
 for /f "tokens=6 delims=[]. " %%G in ('ver') do if %%G lss 16299 goto :version
 %windir%\system32\reg.exe query "HKU\S-1-5-19" 1>nul 2>nul || goto :uac
 setlocal enableextensions
+if /i "%PROCESSOR_ARCHITECTURE%" equ "AMD64" (set "arch=x64") else (set "arch=x86")
 cd /d "%~dp0"
+
+if not exist "*FlightDashboard*.appx" goto :nofiles
+if not exist "*FlightDashboard*.cer" goto :nofiles
+if not exist "*NET.Native.Framework*2.1*.appx" goto :nofiles
+if not exist "*NET.Native.Runtime*2.1*.appx" goto :nofiles
 
 set "PScommand=PowerShell -NoLogo -NoProfile -NonInteractive -InputFormat None -ExecutionPolicy Bypass"
 
@@ -11,8 +17,10 @@ echo ------------------------------------------------------------
 echo Installing XboX Insider Hub
 echo ============================================================
 echo.
-%PScommand% Add-AppxPackage Microsoft.FlightDashboard_469.2003.9001.0_x64__8wekyb3d8bbwe.appx
 %PScommand% Import-Certificate -FilePath Microsoft.FlightDashboard_469.2003.9001.0_x64__8wekyb3d8bbwe.cer -CertStoreLocation Cert:\LocalMachine\Root
+%PScommand% Add-AppxPackage "*NET.Native.Framework*2.1*.appx"
+%PScommand% Add-AppxPackage "*NET.Native.Runtime*2.1*.appx"
+%PScommand% Add-AppxPackage "*FlightDashboard*.appx"
 goto :final
 
 :uac
@@ -30,6 +38,17 @@ exit
 echo.
 echo ------------------------------------------------------------
 echo Only Support For Windows 1803+
+echo ============================================================
+echo.
+echo.
+echo Press any key to Exit
+pause >nul
+exit
+
+:nofiles
+echo.
+echo ------------------------------------------------------------
+echo Somefiles were missing, cannot execute...
 echo ============================================================
 echo.
 echo.
